a29890016f553dd98218bf3dfef61581c3565c37
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2021-03-11 14:51:01 -0500

    final solutions

--- main.js
+++ main.js
@@ -1,85 +1,83 @@
 // Your code here.
 
-const getFirstName = function (unknown) {
-  return unknown.firstName
-}
+getFirstName = (value) => {
+  console.log(value.firstName);
+  return value.firstName;
+};
 
-const getLastName = function (unknown) {
-  return unknown.lastName
-}
+getLastName = (value) => {
+  console.log(value.lastName);
+  return value.lastName;
+};
 
-const getFullName = function (unknown) {
-  return unknown.getFullName = unknown.firstName + ' ' + unknown.lastName
-}
+getFullName = (value) => {
+  return (value.getFullName = value.firstName + " " + value.lastName);
+};
 
-const setFirstName = function (unknown, name) {
-  return  unknown.firstName = name
+setFirstName = (value, name) => {
+  return (value.firstName = name);
+};
 
-}
-// return ${person.firstName} ${person.lastName};
-// string interpolation
+setAge = (value, age) => {
+  return (value.age = age);
+};
 
-const setAge = function (unknown, age) {
-  return unknown.age = age
-}
-
-const giveBirthday = function (person) {
-  if ('age' in person) {
-    person.age++
+giveBirthday = (person) => {
+  if ("age" in person) {
+    // or person.age++
+    person.age = person.age + 1;
   } else {
     person.age = 1;
   }
-}
+};
 
-const marry = function(person1, person2) {
+marry = (person1, person2) => {
   person1.married = true;
   person2.married = true;
 
-  person1.spouseName = person2.firstName + ' ' + person2.lastName;
-  person2.spouseName = person1.firstName + ' ' + person1.lastName;
-}
+  person1.spouseName = person2.firstName + " " + person2.lastName;
+  person2.spouseName = person1.firstName + " " + person1.lastName;
+};
 
-const divorce = function (person1, person2) {
+divorce = (person1, person2) => {
   person1.married = false;
   person2.married = false;
 
-  delete person1.spouseName
-  delete person2.spouseName 
-}
+  delete person1.spouseName && delete person2.spouseName;
+};
 
 // Our code here. Don't touch!
-if (typeof getFirstName === 'undefined') {
-  getFirstName = undefined
+if (typeof getFirstName === "undefined") {
+  getFirstName = undefined;
 }
 
-if (typeof getLastName === 'undefined') {
-  getLastName = undefined
+if (typeof getLastName === "undefined") {
+  getLastName = undefined;
 }
 
-if (typeof getFullName === 'undefined') {
-  getFullName = undefined
+if (typeof getFullName === "undefined") {
+  getFullName = undefined;
 }
 
-if (typeof setFirstName === 'undefined') {
-  setFirstName = undefined
+if (typeof setFirstName === "undefined") {
+  setFirstName = undefined;
 }
 
-if (typeof setAge === 'undefined') {
-  setAge = undefined
+if (typeof setAge === "undefined") {
+  setAge = undefined;
 }
 
-if (typeof giveBirthday === 'undefined') {
-  giveBirthday = undefined
+if (typeof giveBirthday === "undefined") {
+  giveBirthday = undefined;
 }
 
-if (typeof marry === 'undefined') {
-  marry = undefined
+if (typeof marry === "undefined") {
+  marry = undefined;
 }
 
-if (typeof divorce === 'undefined') {
-  divorce = undefined
+if (typeof divorce === "undefined") {
+  divorce = undefined;
 }
-
 
 module.exports = {
   getFirstName,
@@ -90,4 +88,4 @@ module.exports = {
   giveBirthday,
   marry,
   divorce,
-}
+};
